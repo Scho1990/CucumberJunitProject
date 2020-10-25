5b1e3f6e92744bcd2c0b6e18dd54a6536fdc0fae
Scho1990 <it.santosh68@gmail.com>
2020-10-25 16:11:38 +0530

    commit all files

---
+++ CucumberProject/src/test/java/com/stepDefinition/LoginSteps.java
@@ -0,0 +1,54 @@
+package com.stepDefinition;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginSteps {
+	
+	WebDriver driver;
+  
+	@Given("user is on loginPage")
+	public void user_is_on_login_page() {
+	   System.out.println("Step1: User is on LoginPage");
+	   WebDriverManager.chromedriver().setup();
+	   driver= new ChromeDriver();
+	   driver.manage().window().maximize();
+	   driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials");
+	}
+
+	@When("^user enters (.*) and (.*)$")
+	public void user_enters_username_and_password(String uname, String pswd) {
+		System.out.println("Step2: User enters username and pswd");
+	    driver.findElement(By.id("txtUsername")).sendKeys(uname);
+	    driver.findElement(By.id("txtPassword")).sendKeys(pswd);
+	}
+
+	@And("click on login button")
+	public void click_on_login_button() {
+	   System.out.println("Step3: user is clicking on Login Button");
+	   driver.findElement(By.name("Submit")).click();
+	}
+
+	@Then("user should land on home page")
+	public void user_should_land_on_home_page() {
+	    System.out.println("User lands on Home Page");
+	    driver.quit();
+	}
+	
+	@When("user enters credentails using DataTable")
+	public void user_enters_credentails_using_DataTable(DataTable dataTable) {
+		System.out.println("Step2: User enters username and pswd");
+		List<List<String>> data=dataTable.cells();
+	    driver.findElement(By.id("txtUsername")).sendKeys(data.get(0).get(0));
+	    driver.findElement(By.id("txtPassword")).sendKeys(data.get(0).get(1));
+	}
+}
